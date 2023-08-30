f2c12ea26ea38efabe6af5b2484cfdd1e1bac449
markcwy-ra <rocket@markcwy.com>
2023-08-30 20:14:29 +0800

    Initial Commit: Creating Overall Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+// ----- Pages ----- //
+
+import HomePage from "./Pages/HomePage/HomePage";
+import SearchPage from "./Pages/SearchPage/SearchPage";
+import FavouritesPage from "./Pages/FavouritesPage/FavouritesPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import SignUpPage from "./Pages/SignUpPage/SignUpPage";
+
+// ---------- //
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/favourites" element={<FavouritesPage />} />
+    </Routes>
   );
 }
 
